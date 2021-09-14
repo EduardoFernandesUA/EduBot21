aa5eb1b339f0df59b0fbe407da93e951ac9018cc
EduFernandesUA <72972042+EduardoFernandesUA@users.noreply.github.com>
2021-09-14 20:39:05 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+
+app.use(express.static('public'))
+app.use('/files', express.static('files'))
+
+app.get('/', (req, res) => res.send('Server is up.'));
+
+module.exports = () => {
+  app.listen(3000);
+}
